1b428d3845b2865fd3b31f311699e9da456a08fc
saifaldin14 <saifaldindeveloper@gmail.com>
2020-11-14 19:29:01 -0500

    Create api.js

---
+++ server/routes/api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Routes
+router.get('/', (req, res) => {
+
+    BlogPost.find({  })
+        .then((data) => {
+            console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log('error: ', daerrorta);
+        });
+});
+
+router.post('/save', (req, res) => {
+});
+
+
+router.get('/', (req, res) => {
+
+});
+
+
+
+module.exports = router;

f37fcc57f849ca1ce172389fa392cae5ee2774fd
saifaldin14 <saifaldindeveloper@gmail.com>
2020-11-14 19:14:36 -0500

    Run server and client concurrently

--- server/app.js
+++ server/app.js
@@ -1,15 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 5000; //Express uses the same default port as React, so change it to 5000 so it doesn't conflict with React
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const routes = require('./routes/api');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_phi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-app.get('/newEndPoint', (req, res) => {
-  res.send('This is my new end point')
-})
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+
+// HTTP request logger
+app.use(morgan('tiny'));
+app.use('/api', routes);
+
+
+
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));

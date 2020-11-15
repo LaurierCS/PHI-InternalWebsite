5bc7041b8f4d2e2bf72b94c78ba2964941a0ced1
saifaldin14 <saifaldindeveloper@gmail.com>
2020-11-14 20:13:42 -0500

    Connect MongoDB

--- server/app.js
+++ server/app.js
@@ -4,34 +4,34 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-const routes = require('./routes/api');
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_phi', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useCreateIndex:true,
+        useUnifiedTopology:true,
+        useNewUrlParser:true
+    }).then( () => {
+          console.log("Connected To Mongo Db DataBase");
+      }).catch((err) => {
+        console.log("DataBase Connection Error " + err);
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!!!!');
-});
+// mongoose.connection.on('connected', () => {
+//     console.log('Mongoose has been successfully connected');
+// });
 
 // Data parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 
 
 // HTTP request logger
 app.use(morgan('tiny'));
-app.use('/api', routes);
-
-
-
+// app.use('/api', routes);
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));

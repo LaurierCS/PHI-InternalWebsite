1245da61ba786c975046aed840e581691a89caa7
tmanti <tmanti17@gmail.com>
2021-01-29 11:32:36 -0500

    implement /delete/:jobID route

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -2,7 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const mongoose = require('mongoose')
+const Jobs = require('../../models/Job')
 
+
+// @route POST api/jobs/delete
+// @desc delete a job posting from db
+// @access Public
 router.post('/delete/:jobID', (req, res)=>{
-    console.log(req.params['jobID'])
+    //get id from params
+    let id = req.params['jobID']
+    //find and delete from db
+    Jobs.findByIdAndDelete(id, (err, docs)=>{
+        //if err, return status 500
+        if(err) res.status(500)
+
+        //on successful completion, return status 200
+        res.status(200)
+    })
 })

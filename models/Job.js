d8612af6afaae9da3d0ea5170be5affe4d8f0e9d
tmanti <tmanti17@gmail.com>
2021-01-29 11:33:47 -0500

    fix small mistake in placeholder in Jobs.js

--- models/Job.js
+++ models/Job.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Placeholder Job Schema
-const UserSchema = new Schema({
+const JobSchema = new Schema({
 })
 
 module.exports = Job = mongoose.model("Jobs", JobSchema);

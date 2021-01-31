f15a7bbb1b821f8a53a2e4f05cb7bd2ab8f186af
unknown <heard.jacob.7@gmail.com>
2021-01-31 10:08:37 -0500

    Add edit and create routes, finish Job schema

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -19,4 +19,41 @@ router.post('/delete/:jobID', (req, res)=>{
         //on successful completion, return status 200
         res.status(200)
     })
-})
+});
+
+// @route POST api/jobs/delete
+// @desc delete a job posting from db
+// @access Public
+router.post("/edit/:jobID", (req, res) => {
+	let id = req.params["jobID"];
+	let edited = JSON.parse(req.body);
+
+	// TODO: verify that req.body matches Job schema
+
+	Jobs.findByIdAndUpdate({id}, edited, function(err, res) {
+		if(err){
+			res.send(err);
+		} else {
+			res.send(200);
+		}
+	});
+
+});
+
+// @route POST api/jobs/delete
+// @desc delete a job posting from db
+// @access Public
+router.post("/create"), (req, res) => {
+	let id = req.params["jobID"];
+	let entry = JSON.parse(req.body);
+	
+  // TODO: verify that req.body matches Job schema
+
+	Jobs.create(entry, function(err, res) {
+		if(err){
+			res.send(err);
+		} else {
+			res.send(200);
+		}
+	});
+});

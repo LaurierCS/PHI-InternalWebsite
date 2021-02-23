d0ecaf8cfca3e01fab274efb967dfd998468954c
Robert336 <rmazza33@gmail.com>
2021-02-23 15:03:43 -0500

    Fixed publish button and started working on generating Cards from data.
    - Fixed publish button. It now toggles properly. Only downside is that all Card components get refreshed when any publish button is pressed.
    - JobPostingGrid is now a Class instead of a function.
    - JobPostingCard now takes a parameter "props" which is used to pass cardObjects to display unique data needed to make each card.
    
    Done with lots of help from Abdul :)

--- client/src/components/layout/hiring-page/cards/card.js
+++ client/src/components/layout/hiring-page/cards/card.js
@@ -1,92 +1,90 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, Card, CardHeader, CardContent, CardActions, Collapse, IconButton, Typography} from '@material-ui/core';
+import setState from 'react';
+import { makeStyles, Card, CardHeader, CardContent, CardActions, Collapse, IconButton, Typography, Grid} from '@material-ui/core';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import {Inbox, Visibility, Delete, CheckCircle, Edit, ExpandMore, RemoveCircle} from "@material-ui/icons";
 import {Button} from "@material-ui/core";
-import './card.css'
+import './card.css';
 
-//** Requires Material-UI **
 //** under construction **
 
-function JobPostingCard() {
+function JobPostingCard (props){
     const [expanded, setExpanded] = React.useState(false); // default state is collapsed (not expanded)
-    const [published, setPublish] = React.useState(false); // default state is unpublished
-
-    // supposed to handle the publish button toggle (not working rn)
-    const handlePublishingClick = () => {
-        setPublish(!published);
-    };
 
     // handles the card expanding
     // TODO: expand button is not rotating when clicked
-    const handleExpandClick = () => {
+    function handleExpandClick() {
         setExpanded(!expanded);
     };
 
-    return (
-        <Card class="MuiCard-root">
-            <CardHeader
+        return (
+            <Grid item class={"MuiGrid-item"}>
+                <Card class="MuiCard-root">
+                    <CardHeader
 
-                action={
-                    <IconButton aria-label="Edit" title={"Edit Posting"}>
-                        <Edit />
-                    </IconButton>
-                }
-                title="Junior Backend Developer"
-                subheader="Deadline: 02/07/2021"
-            />
-            <CardContent>
+                        action={
+                            <IconButton aria-label="Edit" title={"Edit Posting"}>
+                                <Edit/>
+                            </IconButton>
+                        }
+                        title={props.cardObject.title} // pulls title data from cardInfo array
+                        subheader={"Deadline: " + props.cardObject.deadline} // pulls deadline data from cardInfo array
+                    />
+                    <CardContent>
 
-                <Button>
-                    12 Applications
-                </Button>
-            </CardContent>
-            <CardActions disableSpacing>
+                        <Button title="applications">
+                            {props.cardObject.applications + " applications"}
+                        </Button>
+                    </CardContent>
+                    <CardActions disableSpacing>
 
-                <ToggleButton // Publish button
-                    /*className={clsx(classes.publish, {
-                        [classes.publish]: published,
-                    })}
-                    value="unpublished"
-                    aria-label="publishing"
-                    title={"Unpublished"}
-                    onClick={handlePublishingClick}
-                    */
-                    >
-                     <RemoveCircle />
-                </ToggleButton>
-                <IconButton aria-label="Inbox" title={"View Applications"}>
-                    <Inbox />
-                </IconButton>
-                <IconButton aria-label="View as member" title={"View as member"}>
-                    <Visibility/>
-                </IconButton>
-                <IconButton aria-label={"Delete"} title={"Delete Posting"}>
-                    <Delete/>
-                </IconButton>
-                <IconButton // expand button
-                    className={clsx(
-                        class{"MuiCard-expand"}, {
-                        [class{"MuiCard-expandOpen"}]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMore />
-                </IconButton>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>
-                        test
-                    </Typography>
+                        <ToggleButton
+                            size={"small"}
+                            style={{
+                                borderRadius: 10,
+                                padding: 7
+                            }} //TODO: need to put this in the cards.css file and override the default CSS
+                            selected={props.cardObject.published}
+                            onClick={() => {props.changeStatus(props.cardObject._id)}}
+                        >
+                            {props.cardObject.published ? "Unpublish" : "Publish"}
+                        </ToggleButton>
+                        <IconButton aria-label="Inbox" title={"View Applications"}>
+                            <Inbox/>
+                        </IconButton>
+                        <IconButton aria-label="View as member" title={"View as member"}>
+                            <Visibility/>
+                        </IconButton>
+                        <IconButton aria-label={"Delete"} title={"Delete Posting"}>
+                            <Delete/>
+                        </IconButton>
+                        <IconButton // expand button
+                            className={clsx(
+                                class {
+                                    "MuiCard-expand"
+                                }, {
+                                    [class {
+                                        "MuiCard-expandOpen"
+                                    }]: expanded,
+                                })}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMore/>
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>
+                                test
+                            </Typography>
 
-                </CardContent>
-            </Collapse>
-        </Card>
-    );
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            </Grid>
+        );
 }
-
 export default JobPostingCard;
